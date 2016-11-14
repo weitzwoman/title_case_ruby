3414d208f38a64910a5335ff2633fb60e49a9b7d
Luke Philips and Tracie Weitzman <tracie@>
2016-11-14 11:50:17 -0800

    pass all tests

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,12 +1,16 @@
-# require('rspec')
-# require('pry')
-
 class String
   define_method(:title_case) do
+    # lowercase_words =["and","of","the","or"]
     userArray = self.split()
+    outputArray =[]
     userArray.each() do |word|
-      word.capitalize!()
+        if word.downcase == "of" || word.downcase == "and"
+          outputArray.push(word.downcase())
+        else
+          outputArray.push(word.capitalize())
+        end
+      outputArray.first.capitalize!()
     end
-    userArray
+    outputArray
   end
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -12,4 +12,10 @@ describe('String#title_case') do
   it('capitalizes each word of an array') do
     expect(("hey there you").title_case()).to(eq(["Hey", "There", "You"]))
   end
+  it('lower cases articles, conjunctions, and prepositions in an array') do
+    expect(("john steinbeck's of mice and men").title_case()).to(eq(["John", "Steinbeck's", "of", "Mice", "and", "Men"]))
+  end
+  it('Upper cases articles, conjunctions, and prepositions in an array IF first word in title') do
+    expect(("of mice and men").title_case()).to(eq(["Of", "Mice", "and", "Men"]))
+  end
 end
